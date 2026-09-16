3427b5fffbeeff57308021bca0be5a57c453b1f1
oshevchuk07 <oshevchuk07@gmail.com>
2026-09-16 16:53:36 +0300

    add sqlite and database

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,9 @@
+using GameStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+  public DbSet<Game> Games => Set<Game>();
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,13 @@
 
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
